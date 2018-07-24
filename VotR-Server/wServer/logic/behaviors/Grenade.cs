7af8a516f2c5f39acab1c20c4f62f86730f52e03
Chase Norwood <cntwenty4@gmail.com>
2018-07-24 02:49:44 -0700

    fix

--- VotR-Server/wServer/logic/behaviors/Grenade.cs
+++ VotR-Server/wServer/logic/behaviors/Grenade.cs
@@ -71,7 +71,7 @@ namespace wServer.logic.behaviors
                         Color = new ARGB(color),
                         TargetObjectId = host.Id,
                         Pos1 = target
-                    }, host, null, PacketPriority.Low);
+                    }, host, null, PacketPriority.Normal);
                     host.Owner.Timers.Add(new WorldTimer(1500, (world, t) =>
                     {
                         world.BroadcastPacketNearby(new Aoe()
